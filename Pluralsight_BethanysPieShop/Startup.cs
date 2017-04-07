57eee4d82ad5af32b6b59cb1998c2d2a75843459
thorelore <thorelore@gmail.com>
2017-04-07 14:03:32 -0800

    Continuting with the Demo on Pluralsight.  Module 2

--- Pluralsight_BethanysPieShop/Startup.cs
+++ Pluralsight_BethanysPieShop/Startup.cs
@@ -16,6 +16,9 @@ namespace Pluralsight_BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,15 +26,13 @@ namespace Pluralsight_BethanysPieShop
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //add code to stetup basic http pipeline
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+           
         }
     }
 }
